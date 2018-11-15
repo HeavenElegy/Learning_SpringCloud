07c0f5c50b784eb481401e2a6818b6d37e7340f4
xiaoxi.li <lixiaoxi5@163.com>
2018-11-16 01:24:17 +0800

    1. 修改配置文件中的Unicode到中文 2. 添加两个controller方法。用来作为service应声模板

--- spring-cloud-eureka-client/src/main/java/com/heaven/study/springcloudeurekaclient/controller/MyController.java
+++ spring-cloud-eureka-client/src/main/java/com/heaven/study/springcloudeurekaclient/controller/MyController.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -29,5 +30,18 @@ public class MyController {
 	public Object sayHello(String name) {
 
 		return myService.sayHello(name);
+	}
+
+
+	@RequestMapping(path = "/service.test()")
+	@ResponseBody
+	public Object test() {
+		return "test()";
+	}
+
+	@RequestMapping(path = "/service.test(java.lang.String)")
+	@ResponseBody
+	public Object test(String input) {
+		return "test(String)";
 	}
 }
